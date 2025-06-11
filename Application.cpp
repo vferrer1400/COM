3af814e0a980bb7dbd7dbb8ffc373b339aea6c52
VictorFerrerTR <Victor.Ferrer@thomsonreuters.com>
2025-06-11 09:55:06 -0600

    ExportingObjects

--- Application.cpp
+++ Application.cpp
@@ -1,7 +1,20 @@
 #include "Library.h"
 
+// To compile the application in dev command line
+// cl /W4 Application.cpp Library.def
+
+// Dependency Walker a useful app to review linking problems
+// int main()
+// {
+	// HenCluck();
+	// return 0;
+// }
+
 int main()
 {
-	HenCluck();
-	return 0;
+	IHen * hen = CreateHen();
+	hen->Cluck();
+	hen->Roost();
+
+	delete hen;
 }
--- Library.cpp
+++ Library.cpp
@@ -1,9 +1,42 @@
 #include "Library.h"
-#include <stdio.h>
+//#include <stdio.h>
 
 // To compile as a library in dev command line
 // cl /W4 /LD Library.cpp Library.def
-void __stdcall HenCluck()
+
+// void __stdcall HenCluck()
+// {
+	// printf("C-style cluck!\n");
+// }
+
+#include <windows.h>
+
+#define TRACE OutputDebugString
+
+struct Hen : IHen
 {
-	printf("C-style cluck!\n");
+	Hen()
+	{
+		TRACE("Cheep!\n");
+	}
+	
+	~Hen()
+	{
+		TRACE("Chicken soup!\n");
+	}
+	
+	void __stdcall Cluck()
+	{
+		TRACE("Cluck\n");
+	}
+	
+	void __stdcall Roost()
+	{
+		TRACE("Zzzzzz!\n");
+	}
+};
+
+IHen * __stdcall CreateHen()
+{
+	return new Hen;
 }
