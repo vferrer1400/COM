40162cf7f48e244b75c74e95583872a10ff9daff
VictorFerrerTR <Victor.Ferrer@thomsonreuters.com>
2025-07-14 19:35:44 -0600

    The rise of the smart pointer

--- 2. Exporting Objects/Application.cpp
+++ 2. Exporting Objects/Application.cpp
@@ -3,31 +3,69 @@
 
 #pragma comment(lib, "rpcrt4.lib")
 
+template <typename T>
+class RemoveAddRefRelease : public T
+{
+    ULONG __stdcall AddRef();
+    ULONG __stdcall Release();
+};
+
+template <typename T>
+struct ComPtr
+{
+    T* m_ptr;
+
+public:
+    ComPtr() : m_ptr(0){}
+    ~ComPtr()
+    {
+        if (m_ptr)
+        {
+            m_ptr->Release();
+        }
+    }
+
+    RemoveAddRefRelease<T>* operator ->() const
+    {
+        ASSERT(m_ptr);
+        return static_cast<RemoveAddRefRelease<T>*>(m_ptr);
+    }
+
+    T** GetAddressOf()
+    {
+        return &m_ptr;
+    }
+};
+
 int main()
 {
-    IHen* hen;
-    if(S_OK != CreateHen(&hen))
+    //IHen* hen;
+    ComPtr<IHen> hen;
+    if(S_OK != CreateHen(&hen.m_ptr))
         return 0;
 
     hen->Cluck();
 
-    IHen2* hen2;
+    //IHen2* hen2;
+    ComPtr<IHen2> hen2;
 
     //if (S_OK == hen->QueryInterface(__uuidof(IHen2), reinterpret_cast<void**>(&hen2)))
-    if(S_OK == hen->QueryInterface(&hen2))
+    if(S_OK == hen->QueryInterface(hen2.GetAddressOf()))
     {
         hen2->Forage();
-        hen2->Release();
+        //hen2->Release();
     }
 
-    IOfflineChicken* offline;
-    if (S_OK == hen->QueryInterface(&offline))
+    //IOfflineChicken* offline;
+
+    ComPtr<IOfflineChicken> offline;
+    if (S_OK == hen->QueryInterface(offline.GetAddressOf()))
     {
         offline->Save("filename");
-        offline->Release();
+        //offline->Release();
     }
 
-    hen->Release();
+    //hen->Release();
 
     return 0;
 }
